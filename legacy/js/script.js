cafc5c2c5b7a09cd02ad1f8676667d723c21e3c7
Enindu Alahapperuma <enindu@gmail.com>
2021-11-04 01:01:52 +0530

    Update to v0.1.4

--- legacy/js/script.js
+++ legacy/js/script.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", () => {
     const versions = document.querySelectorAll(".version");
     if(versions.length > 0) {
         versions.forEach((version) => {
-            version.innerText = "0.1.4.legacy-dev";
+            version.innerText = "0.1.4.legacy";
         });
     }
 
