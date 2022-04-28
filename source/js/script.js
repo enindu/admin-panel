f207a398c3c6ecdd8479e976ee9a26c2430eddd8
Enindu Alahapperuma <enindu@gmail.com>
2022-04-29 03:15:34 +0530

    Update to v0.1.7

--- source/js/script.js
+++ source/js/script.js
@@ -6,7 +6,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const versions = document.querySelectorAll('.version');
     if (versions.length > 0) {
         versions.forEach((version) => {
-            version.innerText = '0.1.7-dev';
+            version.innerText = '0.1.7';
         });
     }
 
