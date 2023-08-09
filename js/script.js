4ae4644098c046a658143dcf0aab944f460112b6
Enindu Alahapperuma <enindu@gmail.com>
2023-08-09 21:46:28 +0530

    Update to v0.2.2-dev

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const versions = document.querySelectorAll('.version');
     if (versions.length > 0) {
         versions.forEach((version) => {
-            version.innerText = '0.2.1';
+            version.innerText = '0.2.2-dev';
         });
     }
 
